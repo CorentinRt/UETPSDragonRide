c1f422f0f9b82dbb88152fe7c4d7089425d8e843
CorentinRmt <corentin.remot@gmail.com>
2024-09-22 18:27:01 +0200

    [ADD] create game mode and dragon character

---
+++ Source/TPSDragonRide/private/Character/Dragons/DragonCharacter.cpp
@@ -0,0 +1,32 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TPSDragonRide/Public/Character/Dragons/DragonCharacter.h"
+
+
+// Sets default values
+ADragonCharacter::ADragonCharacter()
+{
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void ADragonCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ADragonCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+// Called to bind functionality to input
+void ADragonCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
---
+++ Source/TPSDragonRide/public/GameMode/DragonRideGameMode.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DragonRideGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TPSDRAGONRIDE_API ADragonRideGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+};
